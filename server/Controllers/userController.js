398ffdd3378394fdd82441204c4cd1c54fd48025
mdsafar <mdsafar2005@gmail.com>
2025-03-01 20:11:42 +0530

    new changes.

--- server/Controllers/userController.js
+++ server/Controllers/userController.js
@@ -1,43 +1,41 @@
-import asyncHandler from 'express-async-handler'
-import { sendToken } from '../utils/jwtToken.js'
-import passport from 'passport'
+import asyncHandler from "express-async-handler";
+import { sendToken } from "../utils/jwtToken.js";
+import passport from "passport";
 
-
-export const googleLogin = passport.authenticate('google', { scope: ['profile', 'email'] })
-
-export const googleCallback = passport.authenticate('google', {
-    successRedirect: "http://localhost:3000",
-    failureRedirect: 'http://localhost:3000/',
+export const googleLogin = passport.authenticate("google", {
+  scope: ["profile", "email"],
 });
 
+export const googleCallback = passport.authenticate("google", {
+  successRedirect: "https://alfred-ai-uyj1.onrender.com/",
+  failureRedirect: "https://alfred-ai-uyj1.onrender.com/",
+});
 
 export const loginSuccess = async (req, res) => {
-    try {
-        const user = req.user
+  try {
+    const user = req.user;
 
-        if (user) {
-            sendToken(user, 200, res)
-        }
-
-    } catch (err) {
-        res.status(400).json({
-            success: false,
-            err
-        })
+    if (user) {
+      sendToken(user, 200, res);
     }
-}
+  } catch (err) {
+    res.status(400).json({
+      success: false,
+      err,
+    });
+  }
+};
 
 export const logout = asyncHandler(async (req, res) => {
+  req.logOut(() => {});
 
-    req.logOut(() => { })
-    
-    res.cookie("token", null, {
-        expires: new Date(Date.now()),
-        httpOnly: true,
-    });
+  res.cookie("token", null, {
+    expires: new Date(Date.now()),
+    httpOnly: true,
+  });
 
-    res.status(200).json({
-        success: true,
-        message: "Logged Out",
-    });
+  res.status(200).json({
+    success: true,
+    message: "Logged Out",
+  });
 });
--- server/MiddleWares/passport.js
+++ server/MiddleWares/passport.js
@@ -1,49 +1,46 @@
-import passport from 'passport';
-import { Strategy as GoogleStrategy } from 'passport-google-oauth20';
-import User from '../Models/userModel.js';
-
+import passport from "passport";
+import { Strategy as GoogleStrategy } from "passport-google-oauth20";
+import User from "../Models/userModel.js";
 
 passport.use(
-    new GoogleStrategy(
-        {
-            clientID: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            callbackURL: '/api/v1/auth/google/callback',
-            scope: ['profile', 'email']
-        },
-        async (accessToken, refreshToken, profile, done) => {
-            try {
-                let user = await User.findOne({ googleId: profile.id });
-                if (!user) {
-                    user = new User({
-                        googleId: profile.id,
-                        username: profile.displayName,
-                        email: profile.emails[0].value,
-                        image: profile.photos[0].value
-                    })
+  new GoogleStrategy(
+    {
+      clientID: process.env.CLIENT_ID,
+      clientSecret: process.env.CLIENT_SECRET,
+      callbackURL: "/api/v1/auth/google/callback",
+      scope: ["profile", "email"],
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        let user = await User.findOne({ googleId: profile.id });
+        if (!user) {
+          user = new User({
+            googleId: profile.id,
+            username: profile.displayName,
+            email: profile.emails[0].value,
+            image: profile.photos[0].value,
+          });
 
-                    await user.save({})
-                }
-
-                return done(null, user);
-
-            } catch (error) {
-                return done(error, false);
-            }
+          await user.save({});
         }
-    )
+
+        return done(null, user);
+      } catch (error) {
+        return done(error, false);
+      }
+    }
+  )
 );
 
-
 passport.serializeUser((user, done) => {
-    done(null, user.id);
+  done(null, user.id);
 });
 
 passport.deserializeUser(async (id, done) => {
-    try {
-        const user = await User.findById(id);
-        done(null, user);
-    } catch (error) {
-        done(error, false);
-    }
+  try {
+    const user = await User.findById(id);
+    done(null, user);
+  } catch (error) {
+    done(error, false);
+  }
 });
